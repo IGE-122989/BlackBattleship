36e5c24e466af433d1906d1272dcb3aaf6490ebf
IGE-122989 <cfrla@iscte-iul.pt>
2026-05-11 14:19:22 +0100

    Correção dos testes Selenium.

--- src/test/java/iscteiul/ista/blackbattleship/MainPage.java
+++ src/test/java/iscteiul/ista/blackbattleship/MainPage.java
@@ -7,16 +7,16 @@ import org.openqa.selenium.support.PageFactory;
 
 // page_url = https://www.jetbrains.com/
 public class MainPage {
-    @FindBy(xpath = "//*[@data-test-marker='Developer Tools']")
+    @FindBy(xpath = "//a[@href='/ides/' and @data-test='main-submenu-item-link']")
     public WebElement seeDeveloperToolsButton;
 
-    @FindBy(xpath = "//*[@data-test='suggestion-action']")
+    @FindBy(xpath = "//a[@data-test='suggestion-link' and @href='/products/']")
     public WebElement findYourToolsButton;
 
-    @FindBy(xpath = "//div[@data-test='main-menu-item' and @data-test-marker = 'Developer Tools']")
+    @FindBy(xpath = "//button[@data-test='main-menu-item-action' and @aria-label='Products: Open submenu']")
     public WebElement toolsMenu;
 
-    @FindBy(css = "[data-test='site-header-search-action']")
+    @FindBy(css = "button[data-test='site-header-search-action']")
     public WebElement searchButton;
 
     public MainPage(WebDriver driver) {
